6d5b7749d74e5c165d4ed6594de36d4196d4a1e6
SniperPig <alexandrurotaru16@gmail.com>
2021-04-11 16:17:33 -0400

    Added GPS Manifest and permissions, it is setting the latitude and altitude

--- app/src/main/java/com/example/operationsunlight/WeatherForecast.java
+++ app/src/main/java/com/example/operationsunlight/WeatherForecast.java
@@ -1,10 +1,17 @@
 package com.example.operationsunlight;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -15,7 +22,11 @@ import org.json.JSONObject;
 import java.util.concurrent.ExecutionException;
 import java.util.Date;
 
-public class WeatherForecast extends AppCompatActivity {
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+public class WeatherForecast extends AppCompatActivity implements LocationListener {
 
     private ProgressDialog progressDialog;
 
@@ -48,14 +59,28 @@ public class WeatherForecast extends AppCompatActivity {
 
     TextView textView, textView2, textView3, textView4, textView5, textView6, textView7, textView8, textView9, textView10, textView11, textView12, textView13;
 
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         // Set to current lat/lon through gps later.
-        lat += "45.5017";
-        lon += "-73.5673";
 
         final_request = url + apiKey + lat + lon;
 
@@ -86,6 +111,16 @@ public class WeatherForecast extends AppCompatActivity {
 
         textView.setText(date.toString());
     }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        lat = "&lat=";
+        lat += location.getLatitude();
+
+        lon = "&lon=";
+        lon += location.getLongitude();
+    }
+
 
     private class GetWeather extends AsyncTask<Void, Void, Void> {
 
