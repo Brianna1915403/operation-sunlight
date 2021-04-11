12575b8fd5b953e29cf1824eb5e83abcd59df449
Brianna <56936663+Brianna1915403@users.noreply.github.com>
2021-04-11 18:22:19 -0400

    Update: Setup the weather fragment for use

---
+++ app/src/main/java/com/example/operationsunlight/ui/weather/WeatherFragment.java
@@ -0,0 +1,26 @@
+package com.example.operationsunlight.ui.weather;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.operationsunlight.R;
+
+public class WeatherFragment extends Fragment {
+    View root;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_weather, container, false);
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
