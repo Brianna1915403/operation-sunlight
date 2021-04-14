3c43210e8164b1ca0d2d8afa7ca13196f9415966
Brianna <56936663+Brianna1915403@users.noreply.github.com>
2021-04-14 15:55:10 -0400

    Fixed the back button issue...

--- app/src/main/java/com/example/operationsunlight/modules/bluetooth/BluetoothFragment.java
+++ app/src/main/java/com/example/operationsunlight/modules/bluetooth/BluetoothFragment.java
@@ -152,7 +152,6 @@ public class BluetoothFragment extends Fragment {
                 connectedThread.write(cmdText);
             }
         });
-
         return root;
     }
 
@@ -313,10 +312,6 @@ public class BluetoothFragment extends Fragment {
         if (createConnectThread != null){
             createConnectThread.cancel();
         }
-        Intent a = new Intent(Intent.ACTION_MAIN);
-        a.addCategory(Intent.CATEGORY_HOME);
-        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(a);
     }
 
 }
